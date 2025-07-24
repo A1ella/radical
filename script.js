e2658f059c3196f78af5b3cc6508bb42e5e2b396
A1ella <kadyrmirgel5432@gmail.com>
2025-07-24 18:42:41 +0600

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-const toggle = document.getElementById('theme-toggle');
-let darkMode = true;
-
-toggle.addEventListener('click', () => {
-  document.body.classList.toggle('light-mode');
-  darkMode = !darkMode;
-  toggle.innerText = darkMode ? '🌗' : '🌞';
-});
